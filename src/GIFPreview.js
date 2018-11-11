97e2093df21d0ddc970c21ddcb712bae37778742
John Harrison <johnharrison@Johns-MacBook-Pro.local>
2018-11-11 17:54:47 -0600

    prettify buttons

--- src/GIFPreview.js
+++ src/GIFPreview.js
@@ -13,8 +13,8 @@ class GIFPreview extends Component {
       <div style={{width: '100vw', height: '100vh', overflow: 'hidden', display: 'flex', alignItems:'center', background: `url(${this.props.gif}) no-repeat center center fixed`,
       backgroundSize: 'cover'}}>
        
-       <button style={{position: 'absolute', margin: '0 auto', bottom: '2rem', left: '50%'}} onClick={this.submitGIF.bind(this)}>Submit GIF</button>
-       <button style={{position: 'absolute', margin: '0 auto', top: '1rem', left: '1rem'}} onClick={this.props.exit.bind(this)}>exit</button>
+       <button style={{position: 'absolute', margin: '0 auto', bottom: '4rem', left: '50%', transform: 'translateX(-50%)', fontSize: '2rem', color: 'white', fontWeight: 'bold', background: '#27ae60', borderRadius: '2rem', border: 'none', outline: 'none', padding: '.5rem 2.5rem'}} onClick={this.submitGIF.bind(this)}>Submit</button>
+       <button style={{position: 'absolute', margin: '0 auto', top: '1rem', left: '1rem', backgroundColor: '#e74c3c', outline: 'none', border: 'none', height: '2rem', width: '2rem', borderRadius: '50%',fontSize: '1rem', color: 'white', fontWeight: 'bold'}} onClick={this.props.exit.bind(this)}>X</button>
       </div>
     );
   }

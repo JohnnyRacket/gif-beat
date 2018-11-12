be52e24a31de0f77d8227bf0678794cf097038e9
John Harrison <johnharrison@Johns-MacBook-Pro.local>
2018-11-11 20:18:49 -0600

    creating the host screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import CaptureGIFScreen from './CaptureGIFScreen';
+import PlayGIFScreen from './PlayGIFScreen';
+import Axios from 'axios';
+import secrets from './secrets/config.json';
 
 class App extends Component {
 
+  state = {
+    captureScreen: false,
+    hostScreen: false,
+    albumIdModal: false,
+    albumId: "Xb6wU"
+  }
+
+  async createAlbum(){
+    //let res = await Axios.post("https://api.imgur.com/3/album",null ,{headers: {Authorization: "Client-ID " + secrets.client_id}});
+    //console.log(res.data.data.id);
+    //this.setState({albumId: res.data.data.id, hostScreen: true});
+    console.log('create album called');
+    this.setState({hostScreen: true});
+  }
+
+  launchAlbumIdModal(){
+    this.setState({albumIdModal: true});
+  }
+
   render() {
     return (
-      <CaptureGIFScreen albumId="123456" />
-    );
-  }
+      <div>
+        {!this.state.captureScreen && !this.state.hostScreen ? (
+        <div>
+          <h1>Welcome to Gif Beat</h1>
+          <button onClick={() => {this.launchAlbumIdModal()}}>Add GIFs with a Code</button>
+          <button onClick={() => {this.createAlbum()}}>Create Party</button>
+          <button>Re-Host Existing Party</button>
+        </div>) : "" }
+        { this.state.captureScreen ? <CaptureGIFScreen albumId={this.state.albumId} /> : ""}
+        { this.state.hostScreen ? <PlayGIFScreen albumId={this.state.albumId} /> : "" }
+        { this.state.albumIdModal ? "album id modal" : ""}
+      </div>
+      
+    );}
 }
 
 export default App;
---
+++ src/PlayGIFScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Axios from 'axios';
+import secrets from './secrets/config.json';
+
+class PlayGIFScreen extends Component {
+
+  state = {
+    video: null,
+    gifs: [],
+    progress: 0
+  }
+
+  componentDidMount(){
+      this.getGIFs();
+  }
+
+  async getGIFs(){
+    let res = await Axios.get(`https://api.imgur.com/3/album/${this.props.albumId}`, {headers: {Authorization: "Client-ID " + secrets.client_id}});
+    let gifs = res.data.data.images;
+    console.log(gifs)
+    this.setState({gifs: gifs});
+  }
+
+  render() {
+    return (
+      <div className="App">
+          {this.state.gifs.length > 0 ? <x-gif src={this.state.gifs[5].link}></x-gif> : <x-gif src="https://i.imgur.com/IUNMjf1.gif"></x-gif>}
+      </div>
+    );
+  }
+}
+
+export default PlayGIFScreen;

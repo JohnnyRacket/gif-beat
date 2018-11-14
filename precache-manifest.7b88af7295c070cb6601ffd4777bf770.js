80c77a4ddb1b37a08530fdebbe59676fe7043646
John Harrison <johnharrison@johns-mbp.attlocal.net>
2018-11-13 21:10:13 -0600

    Updates

---
+++ precache-manifest.7b88af7295c070cb6601ffd4777bf770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70f395e2e55653cde857",
+    "url": "/beat-gif/static/css/main.9431c129.chunk.css"
+  },
+  {
+    "revision": "70f395e2e55653cde857",
+    "url": "/beat-gif/static/js/main.70f395e2.chunk.js"
+  },
+  {
+    "revision": "669d0eca1e7c565055b2",
+    "url": "/beat-gif/static/js/1.669d0eca.chunk.js"
+  },
+  {
+    "revision": "e49255917f18a305e28e",
+    "url": "/beat-gif/static/js/runtime~main.e4925591.js"
+  },
+  {
+    "revision": "4c18f1301eaa2a048256650c5f6dec94",
+    "url": "/beat-gif/static/media/pattern.4c18f130.svg"
+  },
+  {
+    "revision": "f6fd1cad27a9f85c5a09353961289eb5",
+    "url": "/beat-gif/index.html"
+  }
+];

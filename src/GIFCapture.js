5100d91a74b9cc0ed4b2ec34f3389f87ddd8d024
John Harrison <johnharrison@Johns-MacBook-Pro.local>
2018-11-15 23:59:21 -0600

    improvements to ui using loaders

--- src/GIFCapture.js
+++ src/GIFCapture.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as gifshot from 'gifshot';
 import {Circle} from 'rc-progress';
+import loader from './photo-camera.svg';
 
 class GIFCapture extends Component {
 
@@ -9,16 +10,32 @@ class GIFCapture extends Component {
     progress: 0,
     stream: null
   }
+
+  async startCamera(){
+    gifshot.takeSnapShot({
+      'webcamVideoElement': this.state.video,
+      'keepCameraOn': true,
+      'cameraStream': this.state.stream}, async obj => {
+          await this.setState({stream: obj.cameraStream});
+      }); 
+  }
+
+  async stopCamera(){
+    if(this.state.stream !== null) this.state.stream.getTracks().forEach(track => track.stop());
+    this.setState({stream: null});
+  }
   
   async componentDidMount(){
     await this.setState({video: document.getElementById("preview-video")})
-    gifshot.takeSnapShot({
-    'webcamVideoElement': this.state.video,
-    'keepCameraOn': true,
-    'cameraStream': this.state.stream}, async obj => {
-        await this.setState({stream: obj.cameraStream});
-    });
-    
+    this.startCamera();
+    window.onfocus = () => {this.startCamera()};
+    window.onblur = () =>{this.stopCamera()};
+  }
+
+  async componentWillUnmount(){
+    this.stopCamera();
+    window.onfocus = null;
+    window.onblur = null;
   }
 
   captureGIF(){
@@ -33,7 +50,7 @@ class GIFCapture extends Component {
       // the video will not be removed from the DOM.  You will need to handle this yourself.
       // Whether or not you would like the user's camera to stay on after the GIF is created
       // Note: The cameraStream Media object is passed back to you in the createGIF() callback function
-      'keepCameraOn': true,
+
       // Expects a cameraStream Media object
       // Note: Passing an existing camera stream will allow you to create another GIF and/or snapshot without
       //	asking for the user's permission to access the camera again if you are not using SSL
@@ -51,12 +68,15 @@ class GIFCapture extends Component {
   render() {
     return (
         <div className="video-container" style={{zIndex: 100}}>
-            <div style={{width: '100px', position: 'absolute', bottom: '4rem', left: '50%', transform: 'translateX(-50%)'}}>
+            <div style={{padding: '2rem', position: 'absolute',left: '50%', transform: 'translate(-50%, -50%)', top: '33%', zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <img src={loader} className="camera-loader"  />
+            </div>
+            <div style={{width: '100px', position: 'absolute', bottom: '4rem', left: '50%', transform: 'translateX(-50%)', zIndex: 3}}>
                 <Circle percent={this.state.progress * 100} strokeWidth="10" strokeColor="#e74c3c" />
-                <button style={{padding: '2rem', position: 'absolute',left: '50%', transform: 'translate(-50%, -50%)', top: '48%', background: 'transparent', fontWeight: 'bold', color: 'white', border: 'none', fontSize: '2rem', outline: 'none'}} onClick={() => this.captureGIF()}>Rec.</button>
+                {this.state.progress === 0 ? <button style={{ padding: '2rem', position: 'absolute',left: '50%', transform: 'translate(-50%, -50%)', top: '48%', background: 'transparent', fontWeight: 'bold', color: 'white', border: 'none', fontSize: '2rem', outline: 'none'}} onClick={() => this.captureGIF()}>Rec.</button> : "" }
 
             </div>
-            <video className="video" id="preview-video"></video>
+            <video className="video" id="preview-video" style={{zIndex: 2}}></video>
         </div>
     );
   }
